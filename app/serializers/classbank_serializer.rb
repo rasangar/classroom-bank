1e76d13f504ed4d0e0ac3a13739519f314ab24fc
rahul sangar <rasangar15@gmail.com>
2018-08-09 17:31:23 -0400

    added react for the classbanks show page

--- app/serializers/classbank_serializer.rb
+++ app/serializers/classbank_serializer.rb
@@ -1,6 +1,9 @@
 class ClassbankSerializer < ActiveModel::Serializer
     attributes :id, :name, :created_date, :teacher_name
+
     belongs_to :teacher, class_name:"User"
+    has_many :students, class_name:"User"
+    has_many :bank_accounts
 
     def created_date
       object.created_at.strftime("%B %d, %Y")
